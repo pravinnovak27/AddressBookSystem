c7cd513ce0c2bd41d28b499d8e787d10feabebc8
Pravin Novak <pravinnovak2707@gmail.com>
2022-12-05 19:20:19 +0530

    added contact main program

---
+++ main/java/com/bridgelabz/Contact.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+public class Contact {
+
+    public Object getFirstName() {
+        return null;
+    }
+
+    public void setFirstName(String next) {
+    }
+
+    public void setAddress(String next) {
+    }
+
+    public void setCity(String next) {
+    }
+
+    public void setZip(String next) {
+    }
+
+    public void setEmail(String next) {
+    }
+
+    public void setPhoneNumber(String next) {
+    }
+
+}
